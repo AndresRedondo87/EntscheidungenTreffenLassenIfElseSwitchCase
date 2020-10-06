a844e20617d1dbc2e614bf0113d165f855b17dd8
AndresRedondo87 <66962177+AndresRedondo87@users.noreply.github.com>
2020-10-06 11:24:07 +0200

    SWITCH-CASE etwas verbessert(kleinigkeiten, flexible eingaben usw)

--- Program.cs
+++ Program.cs
@@ -40,7 +40,66 @@ namespace EntscheidungenTreffenLassenIfElseSwitchCase
                 Console.ReadKey();
             ////}
 
-            
+
+            //-----------------------------------SWITCH-CASE
+            // Alter
+            int age = 16;
+            Console.WriteLine("Bitte trage deinen Alter ein");
+            age = int.Parse(Console.ReadLine());
+            switch (age)
+            {
+                //...usw, alle sind zu klein.
+                case 9:
+                case 10:
+                case 11:
+                case 12:
+                case 13:
+                case 14:
+                case 15:
+                case 16:
+                case 17:
+                    Console.WriteLine("Zu jung zum Feiern");
+                    break;
+                case 25:
+                    Console.WriteLine("Alles klar, abgehts");
+                    break;
+                // wie Else bei einer If Bedingung
+                default:
+                    Console.WriteLine("Wie alt bist du eigentlich?");
+                    break;
+            }
+
+            if (age <= 15)
+            {
+                Console.WriteLine("Zu jung zum Feiern");
+            }
+            else if (age <= 25)
+            {
+                Console.WriteLine("Alles klar, abgehts");
+            }
+            else
+            {
+                Console.WriteLine("Wie alt bist du eigentlich?");
+            }
+
+            string username = "I am Groot";
+            Console.WriteLine("Bitte trage deinen Benutzernamen ein(Denis,root,)");
+            username = Console.ReadLine();
+
+            switch (username)
+            {
+                case "Denis":
+                    Console.WriteLine("Benutzername ist Denis");
+                    break;
+                case "root":
+                    Console.WriteLine("Benutzername ist root");
+                    break;
+                default:
+                    Console.WriteLine("Benutzername unbekannt");
+                    break;
+            }
+            //--------------------------------------------SWITCH-CASE
+
 
             Console.WriteLine("ENDE");
             Console.ReadKey();
